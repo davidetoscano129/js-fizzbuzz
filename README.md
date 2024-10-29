5acac1f1ff46eeac30f1a65e4a293569ca5e1d7f
davidetoscano129 <toscano.davidew@gmail.com>
2024-10-29 17:51:46 +0100

    Exercise completed

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Far stampare un range di numeri da 1 a 100, sostituendo i seguenti numeri:
 - I multipli di 5 con la parola Buzz
 - Multipli di 3 e di 5 con la parola FizzBuzz
 
+
+## ESECUZIONE
+
+- Denominazione delle variabili Fizz, Buzz e FizzBuzz e del limite del conteggio
+- Avvio ciclo for 
--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-// console.clear()
+// MULTIPLI DI 3
+let fizz = "Fizz";
 
-// // MULTIPLI DI 3
-// let fizz = "Fizz";
+// MULTIPLI DI 5
+let buzz = "Buzz";
 
-// // MULTIPLI DI 5
-// let buzz = "Buzz"; 
-
-// // LIMITE CONTEGGIO 
-// let maxCount = 100; 
+// LIMITE CONTEGGIO 
+let maxCount = 100;
 
 
-// // CICLO DI FOR
-
-// for (let i = 1; i <= maxCount; i++) {
-//     if (i % 3 === 0 && i % 5 === 0) {
-//         console.log(fizz.concat(buzz)); // MULTIPLI DI 3 E 5
-//     } else if (i % 3 === 0) {
-//         console.log(fizz); // MULTIPLI DI 3
-//     } else if (i % 5 === 0) {
-//         console.log(buzz); // MULTIPLI DI 5
-//     } else {
-//         console.log(i); // GLI ALTRI NUMERI
-//     }
-// }
-
-console.log("prova")
+//CICLO DI FOR
+for (let i = 1; i <= maxCount; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(fizz.concat(buzz)); // MULTIPLI DI 3 E 5
+    } else if (i % 3 === 0) {
+        console.log(fizz); // MULTIPLI DI 3
+    } else if (i % 5 === 0) {
+        console.log(buzz); // MULTIPLI DI 5
+    } else {
+        console.log(i); // GLI ALTRI NUMERI
+    }
+}
